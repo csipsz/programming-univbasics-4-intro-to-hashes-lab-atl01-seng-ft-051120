9b19c1bcd369bfa4155dd7e03ec42e1fd76d2024
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-29 15:08:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,12 +23,12 @@ end
 
 def my_hash_creator(key, value)
   lovely_hash = { 
-    key: "value"
+    key: value
   }
 end
 
 def read_from_hash(hash, key)
-  hash[:key]
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
